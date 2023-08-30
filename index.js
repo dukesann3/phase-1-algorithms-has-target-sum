1b96805b1ef89dc695e9d0a3fe21635990d67868
Duke <dukemorise33@gmail.com>
2023-08-29 20:51:57 -0700

    finished target array algorithm practice

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  const seenNumbers = new Set();
+
+  for(let item of array){
+    const complimentary = target - item;
+    if(seenNumbers.has(complimentary)){
+      return true;
+    }
+    seenNumbers.add(item);
+  }
+
+  return false;
+
 }
+
+
+hasTargetSum([2,2,3,3],4);
 
 /* 
   Write the Big O time complexity of your function here
+  O(2n) => O(n)
 */
 
 /* 
   Add your pseudocode here
+  
+  newArray = array.forEach( subtract individual array element from target )
+  for(the length of new array)
+    if new array's individual element is inside of the target array, then return true.
+    If not false outside of the for loop
 */
 
 /*
   Add written explanation of your solution here
+  
+  create array with individual items that are target-arr[i].
+  And see if any of those new array elements are inside of the input array.
+
 */
 
 // You can run `node index.js` to view these console logs
